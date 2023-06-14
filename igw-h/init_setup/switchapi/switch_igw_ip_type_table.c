fad38d70f905934ceaf8bac05700be6ce8e59afc
liweiwei118 <liweiwei118@jd.com>
2023-06-14 19:24:53 +0800

    support ipv6

--- igw-h/init_setup/switchapi/switch_igw_ip_type_table.c
+++ igw-h/init_setup/switchapi/switch_igw_ip_type_table.c
@@ -23,13 +23,10 @@
 #include "bf_igw_ip_type_table.h"
 #include "switch_config.h"
 
-
-void igw_ip_type_table_init() {
+static void internet_out_entry_init() {
 	igwIpTypeKey key;
 	struct in_addr addr;
-
-	igw_ip_type_table_setup();
-
+	
 	memset(&key, 0, sizeof(igwIpTypeKey));
 	key.priority = 0;
 	key.ipv4_isvalid = 1;
@@ -40,4 +37,40 @@ void igw_ip_type_table_init() {
 	key.vxlan_type = VXLAN_TYPE_STD;
 	key.vxlan_type_mask = 0x3;
 	assert(entry_add_with_ip_from_internet_in_hit(&key) == 0);
+}
+
+static void internet_out_between_cluster_dl_entry_init() {
+
+}
+
+static void internet_out_within_cluster_dl_entry_init() {
+	
+}
+
+static void internet_in_entry_init() {
+
+}
+
+static void internet_in_between_cluster_dl_entry_init() {
+
+}
+
+static void internet_in_within_cluster_dl_entry_init() {
+	
+}
+
+void igw_ip_type_table_init() {
+
+	igw_ip_type_table_setup();
+
+	//internet out
+	internet_out_entry_init();
+	internet_out_between_cluster_dl_entry_init();
+	internet_out_within_cluster_dl_entry_init();
+	
+	//internet in
+	internet_in_entry_init();
+	internet_in_between_cluster_dl_entry_init();
+	internet_in_within_cluster_dl_entry_init();
+
 }
