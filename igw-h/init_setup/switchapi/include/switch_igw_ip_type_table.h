70859e736882fa7913cd33aeaed1e2d7b03a5f4c
liweiwei118 <liweiwei118@jd.com>
2023-06-15 11:47:34 +0800

    support ipv6

--- igw-h/init_setup/switchapi/include/switch_igw_ip_type_table.h
+++ igw-h/init_setup/switchapi/include/switch_igw_ip_type_table.h
@@ -12,6 +12,11 @@
 #define VXLAN_TYPE_STD  1
 #define VXLAN_TYPE_JD   2
 
+#define  TOF_EIP_IN  1
+#define  TOF_EIP_OUT 2
+#define  TOF_AZ_IN   3
+#define  TOF_AZ_OUT  4
+
 extern void igw_ip_type_table_init(void);
 
 #endif
--- igw-h/init_setup/switchapi/switch_igw_ip_type_table.c
+++ igw-h/init_setup/switchapi/switch_igw_ip_type_table.c
@@ -23,40 +23,233 @@
 #include "bf_igw_ip_type_table.h"
 #include "switch_config.h"
 
+static uint32_t ipv6_multicast[4] = {0xFF000000, 0, 0, 0};
+static uint32_t ipv6_multicast_mask[4] = {0xFF000000, 0, 0, 0};
+
 static void internet_out_entry_init() {
 	igwIpTypeKey key;
-	struct in_addr addr;
-	
+
+	//1. dst ip is vip, std vxlan, inner_ipv4, tof=0
 	memset(&key, 0, sizeof(igwIpTypeKey));
-	key.priority = 0;
+	key.priority = HIGHEST_PRI;
 	key.ipv4_isvalid = 1;
 	key.ipv4_isvalid_mask = 0x1;
-	inet_aton("0.0.0.0",&addr);	
-	key.ipv4_dstaddr = addr.s_addr;
-	key.ipv4_dstaddr_mask = 0;
+	key.ipv4_dstaddr = switch_cfg.vip;
+	key.ipv4_dstaddr_mask = 0xFFFFFFFF;
+	key.vxlan_isvalid = 1; 
+	key.vxlan_isvalid_mask = 0x1;
+	key.inner_ipv4_isvalid = 1; 
+	key.inner_ipv4_isvalid_mask = 0x1;
+	key.inner_ipv6_isvalid = 0; 
+	key.inner_ipv6_isvalid_mask = 0x1;
 	key.vxlan_type = VXLAN_TYPE_STD;
 	key.vxlan_type_mask = 0x3;
-	assert(entry_add_with_ip_from_internet_in_hit(&key) == 0);
+	key.vxlan_tof = 0;
+	key.vxlan_tof_mask = 0x3;
+	key.ipv6_isvalid = 0;
+	key.ipv6_isvalid_mask = 0x1;
+	assert(entry_add_with_ip_from_internet_out_hit(&key) == 0);
+
+	//2. dst ip is vip, std vxlan, inner_ipv6, tof=0
+	memset(&key, 0, sizeof(igwIpTypeKey));
+	key.priority = HIGHEST_PRI;
+	key.ipv4_isvalid = 1;
+	key.ipv4_isvalid_mask = 0x1;
+	key.ipv4_dstaddr = switch_cfg.vip;
+	key.ipv4_dstaddr_mask = 0xFFFFFFFF;
+	key.vxlan_isvalid = 1; 
+	key.vxlan_isvalid_mask = 0x1;
+	key.inner_ipv4_isvalid = 0; 
+	key.inner_ipv4_isvalid_mask = 0x1;
+	key.inner_ipv6_isvalid = 1; 
+	key.inner_ipv6_isvalid_mask = 0x1;
+	key.vxlan_type = VXLAN_TYPE_STD;
+	key.vxlan_type_mask = 0x3;
+	key.vxlan_tof = 0;
+	key.vxlan_tof_mask = 0x3;
+	key.ipv6_isvalid = 0;
+	key.ipv6_isvalid_mask = 0x1;
+	assert(entry_add_with_ip_from_internet_out_hit(&key) == 0);
 }
 
 static void internet_out_between_cluster_dl_entry_init() {
+	igwIpTypeKey key;
 
+	//1. dst ip is vip, jd vxlan, inner_ipv4, tof=TOF_AZ_OUT
+	memset(&key, 0, sizeof(igwIpTypeKey));
+	key.priority = HIGHEST_PRI;
+	key.ipv4_isvalid = 1;
+	key.ipv4_isvalid_mask = 0x1;
+	key.ipv4_dstaddr = switch_cfg.vip;
+	key.ipv4_dstaddr_mask = 0xFFFFFFFF;
+	key.vxlan_isvalid = 1; 
+	key.vxlan_isvalid_mask = 0x1;
+	key.inner_ipv4_isvalid = 1; 
+	key.inner_ipv4_isvalid_mask = 0x1;
+	key.inner_ipv6_isvalid = 0; 
+	key.inner_ipv6_isvalid_mask = 0x1;
+	key.vxlan_type = VXLAN_TYPE_JD;
+	key.vxlan_type_mask = 0x3;
+	key.vxlan_tof = TOF_AZ_OUT;
+	key.vxlan_tof_mask = 0x3;
+	key.ipv6_isvalid = 0;
+	key.ipv6_isvalid_mask = 0x1;
+	assert(entry_add_with_ip_from_internet_out_dl_hit(&key) == 0);
+
+	//2. dst ip is vip, jd vxlan, inner_ipv6, tof=TOF_AZ_OUT
+	memset(&key, 0, sizeof(igwIpTypeKey));
+	key.priority = HIGHEST_PRI;
+	key.ipv4_isvalid = 1;
+	key.ipv4_isvalid_mask = 0x1;
+	key.ipv4_dstaddr = switch_cfg.vip;
+	key.ipv4_dstaddr_mask = 0xFFFFFFFF;
+	key.vxlan_isvalid = 1; 
+	key.vxlan_isvalid_mask = 0x1;
+	key.inner_ipv4_isvalid = 0; 
+	key.inner_ipv4_isvalid_mask = 0x1;
+	key.inner_ipv6_isvalid = 1; 
+	key.inner_ipv6_isvalid_mask = 0x1;
+	key.vxlan_type = VXLAN_TYPE_JD;
+	key.vxlan_type_mask = 0x3;
+	key.vxlan_tof = TOF_AZ_OUT;
+	key.vxlan_tof_mask = 0x3;
+	key.ipv6_isvalid = 0;
+	key.ipv6_isvalid_mask = 0x1;
+	assert(entry_add_with_ip_from_internet_out_dl_hit(&key) == 0);
 }
 
 static void internet_out_within_cluster_dl_entry_init() {
-	
+	igwIpTypeKey key;
+
+	//dst ip is dl ip, jd vxlan, tof=TOF_EIP_OUT
+	memset(&key, 0, sizeof(igwIpTypeKey));
+	key.priority = HIGHEST_PRI;
+	key.ipv4_isvalid = 1;
+	key.ipv4_isvalid_mask = 0x1;
+	key.ipv4_dstaddr = 0;		//need to modify
+	key.ipv4_dstaddr_mask = 0;  //need to modify
+	key.vxlan_isvalid = 1; 
+	key.vxlan_isvalid_mask = 0x1;
+	key.inner_ipv4_isvalid = 0; 
+	key.inner_ipv4_isvalid_mask = 0;
+	key.inner_ipv6_isvalid = 0; 
+	key.inner_ipv6_isvalid_mask = 0;
+	key.vxlan_type = VXLAN_TYPE_JD;
+	key.vxlan_type_mask = 0x3;
+	key.vxlan_tof = TOF_EIP_OUT;
+	key.vxlan_tof_mask = 0x3;
+	key.ipv6_isvalid = 0;
+	key.ipv6_isvalid_mask = 0x1;
+	assert(entry_add_with_ip_from_internet_out_dl_hit(&key) == 0);
 }
 
 static void internet_in_entry_init() {
+	igwIpTypeKey key;
+	uint8_t *ip6_ptr;
 
+	//1. underlay ipv4, LOWEST_PRI
+	memset(&key, 0, sizeof(igwIpTypeKey));
+	key.priority = LOWEST_PRI;
+	key.ipv4_isvalid = 1;
+	key.ipv4_isvalid_mask = 0x1;
+	key.ipv4_dstaddr = 0;
+	key.ipv4_dstaddr_mask = 0;
+	key.ipv6_isvalid = 0;
+	key.ipv6_isvalid_mask = 0x1;
+	assert(entry_add_with_ip_from_internet_in_hit(&key) == 0);
+
+	//2. underlay ipv6, 0xFF  need drop
+	memset(&key, 0, sizeof(igwIpTypeKey));
+	key.priority = HIGHEST_PRI;
+	key.ipv4_isvalid = 0;
+	key.ipv4_isvalid_mask = 0x1;
+	key.ipv6_isvalid = 1;
+	key.ipv6_isvalid_mask = 0x1;
+	ip6_ptr = (uint8_t *)ipv6_multicast;
+	memcpy(key.ipv6_dstaddr, ip6_ptr, 16);
+	ip6_ptr = (uint8_t *)ipv6_multicast_mask;
+	memcpy(key.ipv6_dstaddr_mask, ip6_ptr, 16);
+	assert(entry_add_with_need_drop(&key) == 0);
+
+	//3. underlay ipv6, LOWEST_PRI
+	memset(&key, 0, sizeof(igwIpTypeKey));
+	key.priority = LOWEST_PRI;
+	key.ipv4_isvalid = 0;
+	key.ipv4_isvalid_mask = 0x1;
+	key.ipv6_isvalid = 1;
+	key.ipv6_isvalid_mask = 0x1;
+	assert(entry_add_with_ip_from_internet_in_hit(&key) == 0);
 }
 
 static void internet_in_between_cluster_dl_entry_init() {
+	igwIpTypeKey key;
 
+	//1. dst ip is vip, jd vxlan, inner_ipv4, tof=TOF_AZ_IN
+	memset(&key, 0, sizeof(igwIpTypeKey));
+	key.priority = HIGHEST_PRI;
+	key.ipv4_isvalid = 1;
+	key.ipv4_isvalid_mask = 0x1;
+	key.ipv4_dstaddr = switch_cfg.vip;
+	key.ipv4_dstaddr_mask = 0xFFFFFFFF;
+	key.vxlan_isvalid = 1; 
+	key.vxlan_isvalid_mask = 0x1;
+	key.inner_ipv4_isvalid = 1; 
+	key.inner_ipv4_isvalid_mask = 0x1;
+	key.inner_ipv6_isvalid = 0; 
+	key.inner_ipv6_isvalid_mask = 0x1;
+	key.vxlan_type = VXLAN_TYPE_JD;
+	key.vxlan_type_mask = 0x3;
+	key.vxlan_tof = TOF_AZ_IN;
+	key.vxlan_tof_mask = 0x3;
+	key.ipv6_isvalid = 0;
+	key.ipv6_isvalid_mask = 0x1;
+	assert(entry_add_with_ip_from_internet_in_dl_hit(&key) == 0);
+
+	//2. dst ip is vip, jd vxlan, inner_ipv6, tof=TOF_AZ_IN
+	memset(&key, 0, sizeof(igwIpTypeKey));
+	key.priority = HIGHEST_PRI;
+	key.ipv4_isvalid = 1;
+	key.ipv4_isvalid_mask = 0x1;
+	key.ipv4_dstaddr = switch_cfg.vip;
+	key.ipv4_dstaddr_mask = 0xFFFFFFFF;
+	key.vxlan_isvalid = 1; 
+	key.vxlan_isvalid_mask = 0x1;
+	key.inner_ipv4_isvalid = 0; 
+	key.inner_ipv4_isvalid_mask = 0x1;
+	key.inner_ipv6_isvalid = 1; 
+	key.inner_ipv6_isvalid_mask = 0x1;
+	key.vxlan_type = VXLAN_TYPE_JD;
+	key.vxlan_type_mask = 0x3;
+	key.vxlan_tof = TOF_AZ_IN;
+	key.vxlan_tof_mask = 0x3;
+	key.ipv6_isvalid = 0;
+	key.ipv6_isvalid_mask = 0x1;
+	assert(entry_add_with_ip_from_internet_in_dl_hit(&key) == 0);
 }
 
 static void internet_in_within_cluster_dl_entry_init() {
-	
+	igwIpTypeKey key;
+
+	//dst ip is dl ip, jd vxlan, tof=TOF_EIP_IN
+	memset(&key, 0, sizeof(igwIpTypeKey));
+	key.priority = HIGHEST_PRI;
+	key.ipv4_isvalid = 1;
+	key.ipv4_isvalid_mask = 0x1;
+	key.ipv4_dstaddr = 0;		//need to modify
+	key.ipv4_dstaddr_mask = 0;  //need to modify
+	key.vxlan_isvalid = 1; 
+	key.vxlan_isvalid_mask = 0x1;
+	key.inner_ipv4_isvalid = 0; 
+	key.inner_ipv4_isvalid_mask = 0;
+	key.inner_ipv6_isvalid = 0; 
+	key.inner_ipv6_isvalid_mask = 0;
+	key.vxlan_type = VXLAN_TYPE_JD;
+	key.vxlan_type_mask = 0x3;
+	key.vxlan_tof = TOF_EIP_IN;
+	key.vxlan_tof_mask = 0x3;
+	key.ipv6_isvalid = 0;
+	key.ipv6_isvalid_mask = 0x1;
+	assert(entry_add_with_ip_from_internet_in_dl_hit(&key) == 0);	
 }
 
 void igw_ip_type_table_init() {
@@ -72,5 +265,4 @@ void igw_ip_type_table_init() {
 	internet_in_entry_init();
 	internet_in_between_cluster_dl_entry_init();
 	internet_in_within_cluster_dl_entry_init();
-
 }
